b2cb06e2d64ca2a1d1396a05afb154bf006237f1
Sunny-Son <insun.son@gmail.com>
2023-04-28 17:05:53 +0900

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
 sfdx force:org:create -f config/project-scratch-def.json -s -d 1 -w 60
 sfdx force:source:push
-sfdx force:user:password:generate --json
+sfdx force:user:password:generate
 sfdx force:org:display --verbose
 sfdx force:org:open
