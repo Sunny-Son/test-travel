bf5c1487f37fadd4156ff4d22cfa47e9e24f2032
Sunny-Son <insun.son@gmail.com>
2023-05-15 16:55:56 +0900

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
 #sfdx force:org:create -f config/project-scratch-def.json -s -d 7 -w 60
-sfdx org:create:scratch -f config/project-scratch-def.json -d -y 7 -w 60 --json
+sfdx org:create:scratch -f config/project-scratch-def.json -d -y 1 -w 60 --json
 sfdx force:source:push
 #sfdx project:deploy:start -d . --json
 sfdx force user password generate
